30eb6d0d8495c5086a4199e2ab846c7f8465cd65
thewisenerd <thewisenerd@protonmail.com>
2015-11-10 02:00:50 +0530

    pocket_mod: make timeout var static
    this ain't going anywhere

--- drivers/misc/pocket_mod.c
+++ drivers/misc/pocket_mod.c
@@ -41,7 +41,7 @@ unsigned pocket_mod_switch = 1;
 unsigned pocket_mod_switch = 0;
 #endif
 
-unsigned int pocket_mod_timeout = 0;
+static unsigned int pocket_mod_timeout = 0;
 static cputime64_t read_time_pre = 0;
 
 int device_is_pocketed(void) {
